0bbb95df10857289e6574f294b94014935c7757a
Luminita Manolache <luminitalumi.manolache@gmail.com>
2020-03-29 08:04:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function currentLine(line) {
     for(var i = 0; i < line.length; i++) {
       currentLineString += "" + (i + 1) + ". " + line[i] + ", ";
       if(i < line.length - 1) {
-        currentLineString += "" + (i + 1) + ". " + line[i] + " ";
+        currentLineString = currentLineString.slice(0, -1);
       }
     }
   return currentLineString;

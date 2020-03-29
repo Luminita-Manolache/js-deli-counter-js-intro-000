ccc2c350bda6c7133e04de814391cfce5be1f07c
Luminita Manolache <luminitalumi.manolache@gmail.com>
2020-03-29 09:08:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,10 +31,11 @@ function currentLine(line) {
     currentLineString += ": ";
     for(var i = 0; i < line.length; i++) {
       currentLineString += " " + (i + 1) + ". " + line[i] + ", ";
-      if(currentLineString === currentLineString.length - 1) {
+      if(i === line.length - 1) {
         currentLineString += " " + (i + 1) + ". " + line[i];
       }
     }
   return currentLineString;
   }
 }
+

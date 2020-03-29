8c4f13750aa4ffee500e2ea7201ec5443e07b39d
Luminita Manolache <luminitalumi.manolache@gmail.com>
2020-03-29 08:49:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,11 +29,8 @@ function currentLine(line) {
     return currentLineString;
   }else {
     currentLineString += ": ";
-    for(var i = 0; i < line.length; i++) {
+    for(var i = 0; i < line.length - 1; i++) {
       currentLineString += " " + (i + 1) + ". " + line[i] + ", ";
-      if(i == line.length - 1) {
-        currentLineString += " " + (i + 1) + ". " + line[i] + "";
-      }
     }
   return currentLineString;
   }

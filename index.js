96b3ac930d161cf2031ba051a81991210428c85b
Luminita Manolache <luminitalumi.manolache@gmail.com>
2020-03-29 08:51:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,8 +29,11 @@ function currentLine(line) {
     return currentLineString;
   }else {
     currentLineString += ": ";
-    for(var i = 0; i < line.length - 1; i++) {
+    for(var i = 0; i < line.length; i++) {
       currentLineString += " " + (i + 1) + ". " + line[i] + ", ";
+      if(i == line.length - 1) {
+        currentLineString += " " + (i + 1) + ". " + line[i] + "";
+      }
     }
   return currentLineString;
   }

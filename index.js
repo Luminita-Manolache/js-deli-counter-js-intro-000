056d7fdfe0c710a5e8abc4ecb016c0422b32af29
Luminita Manolache <luminitalumi.manolache@gmail.com>
2020-03-29 08:27:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function currentLine(line) {
     for(var i = 0; i < line.length; i++) {
       currentLineString += " " + (i + 1) + ". " + line[i] + ", ";
       if(i === line.length - 1) {
-        currentLineString = currentLineString.splice(-1);
+        currentLineString = currentLineString.splice(currentLineString.length - 1);
       }
     }
   return currentLineString;
